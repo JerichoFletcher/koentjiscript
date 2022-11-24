e59c122eace567dbcbc8408abd2704303831a4f7
manuellaiv <89376200+manuellaiv@users.noreply.github.com>
2022-11-24 14:11:36 +0700

    Update expr.py

--- koentjiutil/expr.py
+++ koentjiutil/expr.py
@@ -37,3 +37,114 @@ dfaColon.ignore(' ')
 dfaComma = DFA()
 dfaComma.acceptLiteral(',')
 dfaComma.ignore(' ')
+
+# IF
+dfaIF = DFA()
+dfaIF.acceptLiteral('if')
+
+# ELSE
+dfaELSE = DFA()
+dfaELSE.acceptLiteral('else')
+
+# FOR
+dfaFOR = DFA()
+dfaFOR.acceptLiteral('for')
+
+# WHILE
+dfaWHILE  = DFA()
+dfaWHILE.acceptLiteral('while')
+
+# BREAK
+dfaBREAK = DFA()
+dfaBREAK.acceptLiteral('break')
+
+# CONTINUE
+dfaCONTINUE = DFA()
+dfaCONTINUE.acceptLiteral('continue')
+
+# IN
+dfaIN = DFA()
+dfaIN.start('A')
+dfaIN.accept('E')
+dfaIN.transition('A', ' ', 'B')
+dfaIN.transition('B', ' ', 'B')
+dfaIN.transition('B', 'i', 'C')
+dfaIN.transition('C', 'n', 'D')
+dfaIN.transition('D', ' ', 'E')
+dfaIN.transition('E', ' ', 'E')
+
+# OF
+dfaOF = DFA()
+dfaOF.start('A')
+dfaOF.accept('E')
+dfaOF.transition('A', ' ', 'B')
+dfaOF.transition('B', ' ', 'B')
+dfaOF.transition('B', 'o', 'C')
+dfaOF.transition('C', 'f', 'D')
+dfaOF.transition('D', ' ', 'E')
+dfaOF.transition('E', ' ', 'E')
+
+# SWITCH
+dfaSWITCH = DFA()
+dfaSWITCH.acceptLiteral('switch')
+
+# CASE
+dfaCASE = DFA()
+dfaCASE.acceptLiteral('case')
+
+# DEFAULT
+dfaDEFAULT = DFA()
+dfaDEFAULT.acceptLiteral('default')
+
+# TRY
+dfaTRY = DFA()
+dfaTRY.acceptLiteral('try')
+
+# CATCH
+dfaCATCH = DFA()
+dfaCATCH.acceptLiteral('catch')
+
+# FINALLY
+dfaFINALLY = DFA()
+dfaFINALLY.acceptLiteral('finally')
+
+# FUNCTION
+dfaFUNCTION = DFA()
+dfaFUNCTION.acceptLiteral('function ')
+
+# RETURN
+dfaRETURN = DFA()
+dfaRETURN.acceptLiteral('return')
+dfaRETURN.acceptLiteral('return ')
+
+# THROW
+dfaTHROW = DFA()
+dfaTHROW.acceptLiteral('throw ')
+
+# VAR
+dfaVAR = DFA()
+dfaVAR.acceptLiteral('var ')
+
+# LET
+dfaLET = DFA()
+dfaLET.acceptLiteral('let ')
+
+# CONST
+dfaCONST = DFA()
+dfaCONST.acceptLiteral('const ')
+
+# ID
+dfaID = DFA()
+dfaID.acceptLiteral('id')
+
+# NUM
+dfaNUM = DFA()
+dfaNUM.acceptLiteral('num')
+
+# STR
+dfaSTR = DFA()
+dfaSTR.acceptLiteral('str')
+
+# ARR
+dfaARR = DFA()
+dfaSTR.acceptLiteral('arr')
