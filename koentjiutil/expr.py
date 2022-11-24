d3e959d746c1caa81e4cdca97a5374e4856ac7ee
manuellaiv <89376200+manuellaiv@users.noreply.github.com>
2022-11-24 13:32:07 +0700

    Update expr.py

--- koentjiutil/expr.py
+++ koentjiutil/expr.py
@@ -14,3 +14,24 @@ dfaClosePar.start('A')
 dfaClosePar.accept('B')
 dfaClosePar.transitions('A', (')', 'B'))
 dfaClosePar.ignore(' ')
+
+# {
+dfaOpenBraces = DFA()
+dfaOpenBraces.start('A')
+dfaOpenBraces.accept('B')
+dfaOpenBraces.transitions('A', ('{', 'B'))
+dfaOpenBraces.ignore(' ')
+
+# }
+dfaCloseBraces = DFA()
+dfaCloseBraces.start('A')
+dfaCloseBraces.accept('B')
+dfaCloseBraces.transitions('A', ('}', 'B'))
+dfaCloseBraces.ignore(' ')
+
+# LT
+dfaLT = DFA()
+dfaLT.start('A')
+dfaLT.accept('B')
+dfaLT.transitions('A', (';\n', 'B'))
+
